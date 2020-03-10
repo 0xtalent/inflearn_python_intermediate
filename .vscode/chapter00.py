990ceeaf4b67760509ade2f8274b45749b2f520f
marinehayong <marinehayong@gmail.com>
2020-03-10 19:35:48 +0900

    인프런 파이썬 중급 공부 시작

---
+++ .vscode/chapter00.py
@@ -0,0 +1,6 @@
+# 200310 19:05
+# 인프런 파이썬 중급
+# 가상환경 설정 및 task runner 설정
+
+print("Hello world!")
+print("Hello Python!")
---
+++ .vscode/chapter01_02.py
@@ -0,0 +1,15 @@
+# 200310 19:21
+# 개발 가상 환경 설정 테스트 코드
+
+import pendulum
+from datetime import datetime
+
+pst = pendulum.timezone('America/Los_Angeles')
+ist = pendulum.timezone('Asia/Seoul')
+
+# 타입 확인
+print(type(pst))
+
+# 타임존 시간 출력
+print('Current Date Time in PST = ', datetime.now(pst))
+print('Current Date Time in PST = ', datetime.now(ist))
