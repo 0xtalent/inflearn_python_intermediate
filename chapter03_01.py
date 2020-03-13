bf5643c3ee87ed0142b89243745e7d683d8f7bcc
marinehayong <marinehayong@gmail.com>
2020-03-13 14:32:21 +0900

    매직 메소드 실습 완료

--- chapter03_01.py
+++ chapter03_01.py
@@ -30,3 +30,46 @@ print(n * 100, n.__mul__(100))
 
 print()
 print()
+
+# 클래스 예제1
+class Fruit:
+    def __init__(self, name, price): # 이닛 메소드는 생성자 메소드니까 초기화를 해줘야죠
+        self._name = name
+        self._price = price
+
+    def __str__(self):
+        return 'Fruit Class Info : {} , {}'.format(self._name, self._price)
+
+    def __add__(self, x):
+        return self._price + x._price
+
+    def __ge__(self, x):
+        print('Called >> __ge__ Method.')
+        if self._price >= x._price:
+            return True
+        else:
+            return False
+
+    def __le__(self, x):
+        print('Called >> __le__ Method.')
+        if self._price <= x._price:
+            return True
+        else:
+            return False
+
+    def __sub__(self, x):
+        print('Called >> __sub__ Method.')
+        return self._price - x._price
+
+# 인스턴스 생성
+s1 = Fruit('Orange', 7500)
+s2 = Fruit('Banana', 3000)
+
+# 매직메소드 출력
+print(s1 + s2)
+print(s1 >= s2)
+print(s1 <= s2)
+print(s1 - s2)
+print(s2 - s1)
+print(s1)
+print(s2)
